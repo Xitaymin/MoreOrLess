b5296a6812196203af35c2814bb4e67ac0732f20
Xitaymin <y.barashenko@gmail.com>
2020-11-15 12:41:06 +0200

    Created method for getting random number from range

--- src/ua/training/Model.java
+++ src/ua/training/Model.java
@@ -1,4 +1,14 @@
 package ua.training;
 
 public class Model {
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 100;
+    private int guessedNumber;
+
+    public int getGuessedNumber() {
+        return guessedNumber;
+    }
+    private int getRandomNumber() {
+        return (int)(Math.random()*(MAX_RANGE - MIN_RANGE) + MIN_RANGE);
+    }
 }
