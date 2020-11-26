e347bdfe6bf6bfe9b5fade476fe969ba747dde44
Xitaymin <y.barashenko@gmail.com>
2020-11-26 22:35:09 +0200

    Extracted some methods

--- src/ua/training/Controller.java
+++ src/ua/training/Controller.java
@@ -17,33 +17,49 @@ public class Controller {
         int userInput;
         view.showMessage(View.GREETING);
         while (shouldInputContinue) {
-            while (!scanner.hasNextInt()){
-                view.showMessage(View.WRONG_INPUT_TYPE);
-                scanner.next();
+            checkIfIntValue(scanner);
+            userInput = scanner.nextInt();
+            shouldInputContinue = processUserIntValue(scanner, shouldInputContinue, userInput);
+        }
+    }
+
+    private void checkIfIntValue(Scanner scanner) {
+        while (!scanner.hasNextInt()){
+            view.showMessage(View.WRONG_INPUT_TYPE);
+            scanner.next();
+        }
+    }
+
+    private boolean processUserIntValue(Scanner scanner, boolean shouldInputContinue, int userInput) {
+        if (checkIfUserInputValid(userInput)) {
+            int compairingResult = model.compareUserInputWithGuessedNumber(userInput);
+            if (compairingResult == Model.EQUAL) {
+                endGame(scanner, userInput);
+                shouldInputContinue = false;
+            } else if (compairingResult == Model.LESS) {
+                askUserToContinue(View.LESS_NUMBER);
+            } else {
+                askUserToContinue(View.GREATER_NUMBER);
             }
-                userInput = scanner.nextInt();
-                if (checkIfUserInputValid(userInput)) {
-                    int compairingResult = model.compareUserInputWithGuessedNumber(userInput);
-                    if (compairingResult == Model.EQUAL) {
-                        view.showMessage(View.CONGRATULATION + userInput);
-                        view.showStepCounter(model.getStepCounter());
-                        scanner.close();
-                        shouldInputContinue = false;
-                    } else if (compairingResult == Model.LESS) {
-                        view.showMessageWithRange(View.LESS_NUMBER, model.getMinLimit(), model.getMaxLimit());
-                        view.showHistoryOfAttempts(model.getHistory());
-                    } else {
-                        view.showMessageWithRange(View.GREATER_NUMBER, model.getMinLimit(), model.getMaxLimit());
-                        view.showHistoryOfAttempts(model.getHistory());
-                    }
-                } else {
-                    view.showMessageWithRange(View.INVALID_INPUT, model.getMinLimit(), model.getMaxLimit());
-                    view.showHistoryOfAttempts(model.getHistory());
-                }
-            }
+        } else {
+            askUserToContinue(View.INVALID_INPUT);
+        }
+        return shouldInputContinue;
+    }
+
+    private void endGame(Scanner scanner, int userInput) {
+        view.showMessage(View.CONGRATULATION + userInput);
+        view.showStepCounter(model.getStepCounter());
+        scanner.close();
+    }
+
+    private void askUserToContinue(String compairingResult) {
+        view.showMessageWithRange(compairingResult, model.getMinLimit(), model.getMaxLimit());
+        view.showHistoryOfAttempts(model.getHistory());
     }
 
     // todo let user to change range
+    //todo make view methods more independent from business logic
     boolean checkIfUserInputValid (int userInput) {
         return (userInput<model.getMaxLimit())&&(userInput>model.getMinLimit());
     }
